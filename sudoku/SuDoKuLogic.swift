e64ccdee696f554f89676e9eef8cd8b620d65169
Moinuddin Girach <moinuddingirach@gmail.com>
2021-12-12 13:04:32 +0530

    undo, redo and smoe ui improvement

--- sudoku/SuDoKuLogic.swift
+++ sudoku/SuDoKuLogic.swift
@@ -7,99 +7,191 @@
 
 import Foundation
 
+struct Steps {
+    var row: Int
+    var col: Int
+    var oldValue: Int
+    var newValue: Int
+}
 
-class Sudoku {
+struct Board {
+    let size: Int
+    let defaultValues: Int = 0
+    let steps: [Steps] = []
+    var slots: [[Slots]] = []
+}
+
+struct Slots {
+    var value: Int
+    let isPrefilled: Bool
+    let correctValue: Int
+    let location : Location
+    
+    var isFilled: Bool {
+        return value != 0
+    }
+    var isCorrect: Bool {
+        return correctValue == value
+    }
+}
+
+struct Location {
+    let row: Int
+    let col: Int
+}
+
+class GameManager {
+    
+    subscript(row: Int, col: Int) -> Int {
+        return board!.slots[row][col].value
+    }
+    
+    static let shared: GameManager = {
+       return GameManager()
+    }()
+    
+    func createGame() {
+        let boards = sudoku.createPuzzle(difficulty: 4)
+        createGameBoard(fullboard: boards.fullBoard, puzzleBoard: boards.puzzleBoard)
+    }
+    
+    func resetGame() {
+        steps = []
+        for i in 0..<9 {
+            for j in 0..<9 {
+                if let isPrefiled = board?.slots[i][j].isPrefilled, !isPrefiled {
+                    board?.slots[i][j].value = 0
+                }
+            }
+        }
+    }
+    
+    func fillValue(value: Int, row: Int, col: Int, isUndoRedo: Bool = false) {
+        if let slot = board?.slots[row][col], !(slot.isPrefilled && slot.isFilled) {
+            if !isUndoRedo {
+                let step = Steps(row: row, col: col, oldValue: slot.value, newValue: value)
+                setStep(step: step)
+            }
+            board?.slots[row][col].value = value
+
+        }
+    }
+    
+    func eraseValue(row: Int, col: Int) {
+        fillValue(value: 0, row: row, col: col)
+    }
+    
+    func getHint() -> (value: Int, row: Int, col: Int) {
+        var row = Int(arc4random() % 9)
+        var col = Int(arc4random() % 9)
+        while board!.slots[row][col].isFilled {
+            row = Int(arc4random() % 9)
+            col = Int(arc4random() % 9)
+        }
+        let slot = board!.slots[row][col]
+        fillValue(value: slot.correctValue, row: row, col: col)
+        return (slot.correctValue, row, col)
+    }
+    
+    func undo() -> (value: Int, row: Int, col: Int)? {
+        if (steps.count > undoPointer) && undoPointer >= 0 {
+            let step = steps[undoPointer]
+            fillValue(value: step.oldValue, row: step.row, col: step.col, isUndoRedo: true)
+            undoPointer -= 1
+            return (step.oldValue, step.row, step.col)
+        }
+        return nil
+    }
+    
+    func redo() -> (value: Int, row: Int, col: Int)? {
+        if steps.count > (undoPointer + 1) {
+            let step = steps[undoPointer + 1]
+            fillValue(value: step.newValue, row: step.row, col: step.col, isUndoRedo: true)
+            undoPointer += 1
+            return (step.newValue, step.row, step.col)
+        }
+        return nil
+    }
+    
+    private var board: Board?
+    private var steps: [Steps] = []
+    private var sudoku = SudokuGenerator()
+    private var undoPointer: Int = -1
+    
+    private init() {
+        
+    }
+    
+    private func createGameBoard(fullboard: [[Int]], puzzleBoard: [[Int]]) {
+        board = Board(size: 9)
+        for (indexRow, row) in fullboard.enumerated() {
+            var gameRow = [Slots]()
+            for (indexCol, element) in row.enumerated() {
+                let location = Location(row: indexRow, col: indexCol)
+                let isPrefilled = puzzleBoard[indexRow][indexCol] != 0
+                let slot = Slots(value: puzzleBoard[indexRow][indexCol], isPrefilled: isPrefilled, correctValue: element, location: location)
+                gameRow.append(slot)
+            }
+            board?.slots.append(gameRow)
+        }
+    }
+    
+    private func setStep(step: Steps) {
+        while steps.count > (undoPointer + 1) {
+            steps.removeLast()
+        }
+        steps.append(step)
+        undoPointer += 1
+    }
+}
+
+class SudokuGenerator {
     
     /// reate puzzle
     /// - Parameter difficulty: 1 - 5
     /// - Returns: give sudoku elements array
-    func createPuzzle(difficulty: Int) -> [[Int]] {
-        reset()
-        self.difficulty = difficulty
-        generateGame(grid: &gridPuzzle)
-        gridSolution = gridPuzzle
-        gridProgress = gridPuzzle
-        prepareBoard()
-        return gridPuzzle
+    func createPuzzle(difficulty: Int) -> (fullBoard: [[Int]], puzzleBoard: [[Int]]) {
+        var grid = [[Int]].init(repeating: [Int].init(repeating: 0, count: 9), count: 9)
+        generateGame(grid: &grid)
+        let puzzle = prepareBoard(grid: grid,difficulty: difficulty)
+        return (grid, puzzle)
     }
     
-    /// remove number from the board
-    /// - Parameters:
-    ///   - row: grid row 0 - 8
-    ///   - col: grid col 0 - 8
-    /// - Returns: true if success else false
-    func eraseNumber(row: Int, col: Int) -> Bool {
-        if gridPuzzle[row][col] != 0 {
-            return false
-        } else {
-            gridProgress[row][col] = 0
-            return true
-        }
-    }
-    
-    /// insert number to board
-    /// - Parameters:
-    ///   - number: number to be inserted
-    ///   - row: grid row 0 - 8
-    ///   - col: grid col 0 - 8
-    /// - Returns: true if success else false
-    func addNumber(number: Int, row: Int, col: Int) -> Bool {
-        if gridProgress[row][col] != 0 {
-            return false
-        } else {
-            if gridSolution[row][col] == number {
-                gridProgress[row][col] = number
-                return true
-            } else {
-                return false
-            }
-        }
-    }
-    
+ 
     /// give randoom hint object
     /// - Returns: gives value, row and col for which hint given
-    func getHint() -> (value: Int, row: Int, col: Int)? {
-        if isFull(grid: gridProgress) {
-            return nil
-        }
-        var row = Int(arc4random() % 9)
-        var col = Int(arc4random() % 9)
-        while gridProgress[row][col] != 0 {
-            row = Int(arc4random() % 9)
-            col = Int(arc4random() % 9)
-        }
-        return (gridSolution[row][col], row, col)
-    }
+//    func getHint() -> (value: Int, row: Int, col: Int)? {
+//        if isFull(grid: gridProgress) {
+//            return nil
+//        }
+//        var row = Int(arc4random() % 9)
+//        var col = Int(arc4random() % 9)
+//        while gridProgress[row][col] != 0 {
+//            row = Int(arc4random() % 9)
+//            col = Int(arc4random() % 9)
+//        }
+//        return (gridSolution[row][col], row, col)
+//    }
     
     /// check either all values are filled in game
     /// - Returns: true if game completed else false
-    func isGameCompleted() -> Bool {
-        return isFull(grid: gridProgress)
-    }
+//    func isGameCompleted() -> Bool {
+//        return isFull(grid: gridProgress)
+//    }
     
-    private var gridPuzzle = [[Int]].init(repeating: [Int].init(repeating: 0, count: 9), count: 9)
-    private var gridProgress = [[Int]].init(repeating: [Int].init(repeating: 0, count: 9), count: 9)
-    private var gridSolution = [[Int]].init(repeating: [Int].init(repeating: 0, count: 9), count: 9)
     private var numlist = [1,2,3,4,5,6,7,8,9]
     private var counter = 1
-    private var difficulty = 5
-
-    private func reset() {
-        gridPuzzle = [[Int]].init(repeating: [Int].init(repeating: 0, count: 9), count: 9)
-        gridProgress = [[Int]].init(repeating: [Int].init(repeating: 0, count: 9), count: 9)
-        gridSolution = [[Int]].init(repeating: [Int].init(repeating: 0, count: 9), count: 9)
-    }
     
-    private func isValid(grid: [[Int]]) -> Bool {
-        for i in 0..<9 {
-            for j in 0..<9 {
-                if !(grid[i][j] == 0 || grid[i][j] == gridSolution[i][j]) {
-                    return false
-                }
-            }
-        }
-        return true
-    }
+//    private func isValid(grid: [[Int]]) -> Bool {
+//        for i in 0..<9 {
+//            for j in 0..<9 {
+//                if !(grid[i][j] == 0 || grid[i][j] == gridSolution[i][j]) {
+//                    return false
+//                }
+//            }
+//        }
+//        return true
+//    }
     
     private func getCol(grid: [[Int]], col: Int) -> [Int] {
         var tempArr = [Int]()
@@ -227,7 +319,9 @@ class Sudoku {
         return square
     }
     
-    private func prepareBoard() {
+    private func prepareBoard(grid: [[Int]], difficulty: Int) -> [[Int]] {
+        var gridPuzzle = grid
+        var difficulty = difficulty
         while difficulty > 0 {
             var row = Int(arc4random() % 9)
             var col = Int(arc4random() % 9)
@@ -246,5 +340,6 @@ class Sudoku {
                 difficulty -= 1
             }
         }
+        return gridPuzzle
     }
 }
--- sudoku/ViewController.swift
+++ sudoku/ViewController.swift
@@ -9,22 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let a = Sudoku()
+    let a = GameManager.shared
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let b = a.createPuzzle(difficulty: 5)
+        a.createGame()
         var x = 10
         var y = 100
         for i in 0..<9 {
             for j in 0..<9 {
             let lbl = UITextField(frame: CGRect(x: x, y: y, width: 30, height: 30 ))
                 lbl.delegate = self
-                lbl.textColor = b[i][j] == 0 ? .black : .blue
+                lbl.textColor = a[i, j] == 0 ? .black : .blue
                 lbl.textAlignment = .center
                 lbl.keyboardType = .numberPad
-                lbl.text = b[i][j] == 0 ? "" : "\(b[i][j])"
-                lbl.isUserInteractionEnabled = b[i][j] == 0
+                lbl.text = a[i,j] == 0 ? "" : "\(a[i, j])"
+                lbl.isUserInteractionEnabled = a[i, j] == 0
                 lbl.layer.borderColor = UIColor.black.cgColor
                 lbl.layer.borderWidth = 1
                 lbl.tag = i * 10 + j
@@ -34,23 +34,82 @@ class ViewController: UIViewController {
             x = 10
             y += (30 + (i % 3 == 2 ? 2 : 0))
         }
+        
+        let buttonUndo = UIButton(type: .custom)
+        buttonUndo.frame = CGRect(x: 10, y: y + 10, width: 70, height: 30)
+        buttonUndo.setTitle("undo", for: .normal)
+        buttonUndo.setTitleColor(.black, for: .normal)
+        buttonUndo.addTarget(self, action: #selector(undo), for: .touchUpInside)
+        self.view.addSubview(buttonUndo)
+        
+        
+        let buttonRedo = UIButton(type: .custom)
+        buttonRedo.frame = CGRect(x: 100, y: y + 10, width: 70, height: 30)
+        buttonRedo.setTitle("redo", for: .normal)
+        buttonRedo.setTitleColor(.black, for: .normal)
+        buttonRedo.addTarget(self, action: #selector(redo), for: .touchUpInside)
+        self.view.addSubview(buttonRedo)
+        
+        let buttonErase = UIButton(type: .custom)
+        buttonErase.frame = CGRect(x: 200, y: y + 10, width: 70, height: 30)
+        buttonErase.setTitle("erase", for: .normal)
+        buttonErase.setTitleColor(.black, for: .normal)
+        buttonErase.addTarget(self, action: #selector(eraase), for: .touchUpInside)
+        self.view.addSubview(buttonErase)
     }
 
+    @objc func undo(sender: UIButton) {
+        if let val = a.undo() {
+            if let text = self.view.viewWithTag(val.row * 10 + val.col) as? UITextField {
+                text.text = val.value == 0 ? "" : "\(val.value)"
+            }
+        }
+    }
+    
+    @objc func redo(sender: UIButton) {
+        if let val = a.redo() {
+            if let text = self.view.viewWithTag(val.row * 10 + val.col) as? UITextField {
+                text.text = val.value == 0 ? "" : "\(val.value)"
+            }
+        }
+    }
+    
+    @objc func eraase(sender: UIButton) {
+        if let tag = focousedText?.tag {
+            let col = tag % 10
+            let row = (tag - col) / 10
+            a.eraseValue(row: row, col: col)
+            if let text = self.view.viewWithTag(row * 10 + col) as? UITextField {
+                text.text = ""
+            }
+        }
+    }
+    
+    private var focousedText: UITextField?
 
 }
 
 extension ViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        focousedText = textField
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        focousedText = nil
+        return true
+    }
+    
     public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let col = textField.tag % 10
         let row = (textField.tag - col) / 10
         if let updatedString = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) {
-            if let num = Int(updatedString), num > 0 {
-                let valid = a.addNumber(number: num, row: row, col: col)
-                return valid
+            if let num = Int(updatedString), num > 0 && num < 10 {
+                a.fillValue(value: num, row: row, col: col)
+            } else {
+                return false
             }
 
         }
         return true
     }
 }
-
